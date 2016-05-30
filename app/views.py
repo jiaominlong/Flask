0a5af0625f7a58694ea1f00f2dc7bac31c85f5b5
jml <jiaominlong@outlook.com>
2016-05-30 16:44:22 +0800

    第一个文件可运行文件

--- app/views.py
+++ app/views.py
@@ -1,6 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template,request
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    token = 'jiaominlong'
+    signature = request.args.get('signature')
+    timestamp = request.args.get('timestamp')
+    nonce = request.args.get('nonce')
+    echostr = request.args.get('echostr')
+
+    s = [token, timestamp, nonce]
+    s.sort()
+    s = ''.join(s)
+    if s == signature:
+        return True
+    else:
+        print('fiall')
+        #return render_template('index.html')
+
+@app.route('/weixin')
+def weixin():
+    pass
